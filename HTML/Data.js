73bbe9897d4269ea766cfb4e486d2afab0cecd16
Arthur Laudien <Arthur.Laudien@cpe.fr>
2020-10-23 12:10:28 +0200

    Oui

--- HTML/Data.js
+++ HTML/Data.js
@@ -1,1 +1,21 @@
-T=getinfo()
+var T;
+function info(){
+    let Nom = new URLSearchParams(window.location.search).get("Nom");
+    let Prenom = new URLSearchParams(window.location.search).get("Prenom");
+    let Mail = new URLSearchParams(window.location.search).get("Mail");
+    let Tel = new URLSearchParams(window.location.search).get("Tel");
+    let date_depart = new URLSearchParams(window.location.search).get("Date_de_depart");
+    let date_retour = new URLSearchParams(window.location.search).get("Date_de_retour");
+    let nombre_adultes = new URLSearchParams(window.location.search).get("Nombre_adultes");
+    let nombre_enfants = new URLSearchParams(window.location.search).get("Nombre_enfants");
+    let Dj = new URLSearchParams(window.location.search).get("Dj");
+    console.log(Nom);
+    console.log(Prenom);
+    console.log(Mail);
+    console.log(Tel);
+    console.log(date_depart);
+    console.log(date_retour);
+    console.log(nombre_adultes);
+    console.log(nombre_enfants);
+    console.log(Dj);
+    }
--- HTML/Réservation.js
+++ HTML/Réservation.js
@@ -1,8 +1,8 @@
 var Nom = Text;
-var Prénom= Text;
+var Prenom= Text;
 var Mail = Text;
 var Tel = Int8Array;
-var date_départ = Int8Array;
+var date_depart = Int8Array;
 var date_retour = Int8Array;
 var nombre_adultes = Int8Array;
 var nombre_enfants = Int8Array;
@@ -13,16 +13,16 @@ var Dj = Boolean;
 
 function getinfo(){
     Nom = document.getElementById("Nom").value;
-    Prénom = document.getElementById("Prénom").value;
+    Prenom = document.getElementById("Prenom").value;
     Mail = document.getElementById("Mail").value;
     Tel = document.getElementById("Tel").value;
-    date_départ = document.getElementById("date_départ").value;
+    date_depart = document.getElementById("date_depart").value;
     date_retour = document.getElementById("date_retour").value;
     nombre_adultes = document.getElementById("nombre_adultes").value;
     nombre_enfants = document.getElementById("nombre_enfants").value;
     Dj = document.forms["form"]["Dj"].value;
-    console.log (Dj);
-    T=[Nom,Prénom,Mail,Tel,date_départ,date_retour,nombre_adultes,nombre_enfants,Dj];
+    console.log (date_retour);
+    T=[Nom,Prenom,Mail,Tel,date_depart,date_retour,nombre_adultes,nombre_enfants,Dj];
     return T; 
 }
 
